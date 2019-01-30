d8b213fd8e3965f8dbc1d92207812d8539877d01
Vlad Kozyrev <Vlad.Kozyrev@artezio.com>
2019-01-30 14:44:26 +0300

    Начата работа над каталогом.

--- src/components/Catalog/Catalog.vue
+++ src/components/Catalog/Catalog.vue
@@ -2,25 +2,225 @@
     <div>
         <div class="container">
             <div class="row omega">
-                <div class="col-md-12 bg-danger">
-                    Тут может быть заказ?
-                </div>   
-
+                <div class="col-md-3 food-body">
+                    <img src="https://image.flaticon.com/icons/svg/1365/1365596.svg"/>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 counter">
+                                    <p>
+                                        0
+                                    </p>
+                                    <p>
+                                        0
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 food-body">
+                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 counter">
+                                    <p>
+                                        0
+                                    </p>
+                                    <p>
+                                        0
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <div class="col-md-3 food-body">
+                    <img src="https://image.flaticon.com/icons/png/512/1365/1365576.png"/>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 counter">
+                                    <p>
+                                        0
+                                    </p>
+                                    <p>
+                                        0
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+                <div class="col-md-3 food-body">
+                    <img src="https://image.flaticon.com/icons/svg/1408/1408052.svg"/>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 counter">
+                                    <p>
+                                        0
+                                    </p>
+                                    <p>
+                                        0
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
             </div>
         </div>
 
         <div class="container">
             <div class="row">
-                <div class="col-md-9 catalog bg-primary">
+                <div class="col-md-10 catalog bg-primary">
                         <div class="collapse burger" id="demo">
-                            <div class="card">
-                                Hello There parthner
-                                <div class="card-header">Header</div>
-                                <div class="card-body">Content</div> 
-                                <div class="card-footer">Footer</div>
+                            <div class="container text-center">
+                                <div id="recipeCarousel" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner" role="listbox">
+                                        <div class="carousel-item row row-eq-height catalog-items active">
+                                            <div class="col-3 float-left">
+                                                 <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром
+                                                    </p>
+                                                    <p class="item-price">
+                                                        Цена - 1.6 BYN
+                                                    </p>
+                                                    <div>
+                                                        <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-3 float-left">
+                                                <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром
+                                                    </p>
+                                                    <p class="item-price">
+                                                        Цена - 1.6 BYN
+                                                    </p>
+                                                    <div>
+                                                        <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-3 float-left"> <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром
+                                                    </p>
+                                                    <p class="item-price">
+                                                        Цена - 1.6 BYN
+                                                    </p>
+                                                    <div>
+                                                        <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-3 float-left"> <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром 
+                                                    </p>
+                                                    <div class="price">
+                                                        <p class="item-price">
+                                                            Цена - 1.6 BYN
+                                                        </p>
+                                                            <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item row row-eq-height catalog-items">
+                                            <div class="col-3 float-left">
+                                                 <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром
+                                                    </p>
+                                                    <p class="item-price">
+                                                        Цена - 1.6 BYN
+                                                    </p>
+                                                    <div>
+                                                        <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-3 float-left">
+                                                <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром
+                                                    </p>
+                                                    <p class="item-price">
+                                                        Цена - 1.6 BYN
+                                                    </p>
+                                                    <div>
+                                                        <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-3 float-left"> <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром
+                                                    </p>
+                                                    <p class="item-price">
+                                                        Цена - 1.6 BYN
+                                                    </p>
+                                                    <div>
+                                                        <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-3 float-left"> <div class="items-body">
+                                                    <p>
+                                                        Бургер с сыром
+                                                    </p>
+                                                    <img src="https://image.flaticon.com/icons/svg/1046/1046784.svg"/>
+                                                    <p class="item-decsription">
+                                                        Гамбурегр с сыром 
+                                                    </p>
+                                                    <div class="price">
+                                                        <p class="item-price">
+                                                            Цена - 1.6 BYN
+                                                        </p>
+                                                            <button class="btn btn-danger">Add</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev catalog-nav-left" href="#recipeCarousel" role="button" data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next catalog-nav-right" href="#recipeCarousel" role="button" data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
                             </div>
-                           
                         </div>
+                        
                         <div class="collapse salad" id="demo2">
                             <div class="card">
                                 <div class="card-header">здесь изображение </div>
@@ -50,7 +250,7 @@
                             </div>
                         </div>
                 </div>
-                <div class="col-md-3 option">
+                <div class="col-md-2 option">
                     <div class="option-element">
                         <a href="#demo" @click="toggle" class="plead" data-toggle="collapse" data-type="burger">
                             Бургеры
@@ -80,7 +280,6 @@
                 </div>
             </div>
         </div>
-        Дарова нафиг
     </div>
 </template>
 <script>
@@ -113,22 +312,60 @@ export default {
 </script>
 <style scoped>
 .omega {
+    margin-top: 5%;
     margin-bottom: 10%;
 }
+.food-body img {
+    width: 50%;
+    margin-bottom: 3%; 
+}
+.food-body p {
+    font-size: 25px;
+    width: 35px;
+    border: 2px black;
+    background: rgb(0, 0, 0);
+    color: white;
+    display: inline-block;
+    margin: 0 5px 0 5px;
+}
+
 .option, .catalog, .drinks, .sweat, .dishes {
     padding: 0;
     text-align: center
 }
+.catalog-items {
+    margin-top: 5%;
+    height: 100%;
+}
 .option-element {
-    height: 50px;
+    height: 70px;
     width: 100%;
-    font-size: 22px;
-    border-bottom: 2px black;
+    text-align: center;
+}
+.option-element a {
+    font-size: 25px;
+    color: black;
+}
+.option-element a:hover {
+    text-decoration: none;
 }
 .collapse {
     width: 100%;
     height: 100%;
     text-align: center;
+}
+.catalog-nav-left {
+    left: -60px;
+}
+.catalog-nav-right {
+    right: -60px;
+}
+.items-body img {
+    width: 70%;
+}
+
+.price {
+    vertical-align: bottom;
 }
 .card {
     width: 20%;
